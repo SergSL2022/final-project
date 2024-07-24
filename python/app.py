ed3e4454269998db9eb09354d7ca3d23699a3046
SergSL2022 <ZGravity@ukr.net>
2024-07-24 04:00:10 +0300

    Update python app

--- python/app.py
+++ python/app.py
@@ -10,22 +10,5 @@ def root():
     }
     return jsonify(data), 200, {'Content-Type': 'application/json'}
 
-@app.route('/test')
-def test():
-    data = {
-        'status': 200,
-        'message': 'Ok. Test successful'
-    }
-    return jsonify(data), 200, {'Content-Type': 'application/json'}
-
-@app.route('/healthz')
-def healthz():
-    data = {
-        'status': 200,
-        'message': 'Healthz. Ok'
-    }
-    return jsonify(data), 200, {'Content-Type': 'application/json'}
-
-
 if __name__ == '__main__':
     app.run(debug=True)
